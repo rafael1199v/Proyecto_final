cd50451bf89b9dc45763c9c1919d0cc454ad93bd
DanyElAlgo <64906201+DanyElAlgo@users.noreply.github.com>
2023-12-08 08:24:58 -0400

    arreglo SNAKE_CASE
    la poblacion ahora funciona según la norma de uso de SNAKE_CASE

--- Script Poblacion.sql
+++ Script Poblacion.sql
@@ -1,170 +1,165 @@
 --CREDENCIALES
--- Declarar variables
+-- DECLARAR VARIABLES
 DECLARE
-    v_nombre_usuario VARCHAR2(50);
-    v_correo VARCHAR2(100);
-    v_contrasenha VARCHAR2(20);
+    V_NOMBRE_USUARIO VARCHAR2(50);
+    V_CORREO VARCHAR2(100);
+    V_CONTRASENHA VARCHAR2(20);
 BEGIN
-    -- Iniciar bucle FOR
-    FOR i IN 1..20 LOOP
-        -- Generar datos aleatorios
-        v_nombre_usuario := 'Usuario' || TO_CHAR(i);
-        v_correo := 'usuario' || TO_CHAR(i) || '@example.com';
-        v_contrasenha := DBMS_RANDOM.STRING('X', 8); -- Generar contraseña aleatoria de 8 caracteres
+    -- INICIAR BUCLE FOR
+    FOR I IN 1..20 LOOP
+        -- GENERAR DATOS ALEATORIOS
+        V_NOMBRE_USUARIO := 'USUARIO' || TO_CHAR(I);
+        V_CORREO := 'USUARIO' || TO_CHAR(I) || '@EXAMPLE.COM';
+        V_CONTRASENHA := DBMS_RANDOM.STRING('X', 8); -- GENERAR CONTRASEï¿½A ALEATORIA DE 8 CARACTERES
 
-        -- Insertar datos en la tabla
-        INSERT INTO CREDENCIALES (ID, Nombre_usuario, Correo, Contrasenha) VALUES (CREDENCIALES_SEQ.NEXTVAL, v_nombre_usuario, v_correo, v_contrasenha);
+        -- INSERTAR DATOS EN LA TABLA
+        INSERT INTO CREDENCIALES (ID, NOMBRE_USUARIO, CORREO, CONTRASENHA) VALUES (CREDENCIALES_SEQ.NEXTVAL, V_NOMBRE_USUARIO, V_CORREO, V_CONTRASENHA);
     END LOOP;
 
-    -- Commit para confirmar las inserciones
+    -- COMMIT PARA CONFIRMAR LAS INSERCIONES
     COMMIT;
 END;
 /
 
 --USUARIO
--- Utilizando la secuencia USUARIO_SEQ para generar valores de ID
--- y valores aleatorios para la columna ID_CREDENCIALES
+-- UTILIZANDO LA SECUENCIA USUARIO_SEQ PARA GENERAR VALORES DE ID
+-- Y VALORES ALEATORIOS PARA LA COLUMNA ID_CREDENCIALES
 INSERT INTO USUARIO (ID, NOMBRE, EDAD, UBICACION, TELEFONO, ID_CREDENCIALES)
 SELECT USUARIO_SEQ.NEXTVAL,
-       'Usuario' || USUARIO_SEQ.CURRVAL AS NOMBRE,
-       DBMS_RANDOM.value(18, 25) AS EDAD,
-       'Ubicacion' || USUARIO_SEQ.CURRVAL AS UBICACION,
-       LPAD(DBMS_RANDOM.value(10000000, 99999999), 8, '0') AS TELEFONO,
+       'USUARIO' || USUARIO_SEQ.CURRVAL AS NOMBRE,
+       DBMS_RANDOM.VALUE(18, 25) AS EDAD,
+       'UBICACION' || USUARIO_SEQ.CURRVAL AS UBICACION,
+       LPAD(DBMS_RANDOM.VALUE(10000000, 99999999), 8, '0') AS TELEFONO,
        USUARIO_SEQ.CURRVAL
-FROM dual
+FROM DUAL
 CONNECT BY LEVEL <= 20;
 
 
 --USUARIO_AMIGO
--- Utilizando la secuencia USUARIO_AMIGO_SEQ para generar valores de ID
+-- UTILIZANDO LA SECUENCIA USUARIO_AMIGO_SEQ PARA GENERAR VALORES DE ID
 INSERT INTO USUARIO_AMIGO (ID, ID_USUARIO, ID_AMIGO, FECHA_AMISTAD)
 SELECT USUARIO_AMIGO_SEQ.NEXTVAL,
-       DBMS_RANDOM.value(1, 20) AS ID_USUARIO,
-       DBMS_RANDOM.value(1, 20) AS ID_AMIGO,
+       DBMS_RANDOM.VALUE(1, 20) AS ID_USUARIO,
+       DBMS_RANDOM.VALUE(1, 20) AS ID_AMIGO,
        SYSDATE AS FECHA_AMISTAD
-FROM dual
+FROM DUAL
 CONNECT BY LEVEL <= 10;
 
 
 --LISTA_TEMAS
 DECLARE
-    v_id NUMBER;
-    v_tema_publicacion VARCHAR2(50);
-    v_tipo_publicacion VARCHAR2(20);
+    V_ID NUMBER;
+    V_TEMA_PUBLICACION VARCHAR2(50);
+    V_TIPO_PUBLICACION VARCHAR2(20);
 BEGIN
-    -- Iniciar bucle FOR
-    FOR i IN 1..3 LOOP
-        -- Generar datos para "no importante"
-        v_id := i;
-        v_tema_publicacion := 'Deportes';
-        v_tipo_publicacion := 'No Importante';
+    -- INICIAR BUCLE FOR
+    FOR I IN 1..3 LOOP
+        -- GENERAR DATOS PARA "NO IMPORTANTE"
+        V_ID := I;
+        V_TEMA_PUBLICACION := 'DEPORTES';
+        V_TIPO_PUBLICACION := 'NO IMPORTANTE';
 
-        -- Insertar datos en la tabla
-        INSERT INTO LISTA_TEMAS (ID, TEMA_PUBLICACION, TIPO_PUBLICACION) VALUES (v_id, v_tema_publicacion, v_tipo_publicacion);
+        -- INSERTAR DATOS EN LA TABLA
+        INSERT INTO LISTA_TEMAS (ID, TEMA_PUBLICACION, TIPO_PUBLICACION) VALUES (V_ID, V_TEMA_PUBLICACION, V_TIPO_PUBLICACION);
 
-        -- Generar datos para "importante"
-        v_id := i + 3;
-        v_tipo_publicacion := 'Importante';
+        -- GENERAR DATOS PARA "IMPORTANTE"
+        V_ID := I + 3;
+        V_TIPO_PUBLICACION := 'IMPORTANTE';
 
-        -- Insertar datos en la tabla
-        INSERT INTO LISTA_TEMAS (ID, TEMA_PUBLICACION, TIPO_PUBLICACION) VALUES (v_id, v_tema_publicacion, v_tipo_publicacion);
+        -- INSERTAR DATOS EN LA TABLA
+        INSERT INTO LISTA_TEMAS (ID, TEMA_PUBLICACION, TIPO_PUBLICACION) VALUES (V_ID, V_TEMA_PUBLICACION, V_TIPO_PUBLICACION);
     END LOOP;
 
-    -- Commit para confirmar las inserciones
+    -- COMMIT PARA CONFIRMAR LAS INSERCIONES
     COMMIT;
 END;
 /
 
 
 --LISTA_HASHTAG
--- Utilizando la secuencia LISTA_HASHTAG_SEQ para generar valores de ID
--- y generando nombres de hashtags aleatorios
+-- UTILIZANDO LA SECUENCIA LISTA_HASHTAG_SEQ PARA GENERAR VALORES DE ID
+-- Y GENERANDO NOMBRES DE HASHTAGS ALEATORIOS
 INSERT INTO LISTA_HASHTAG (ID, NOMBRE_HASHTAG)
 SELECT LISTA_HASHTAG_SEQ.NEXTVAL,
-       'Hashtag' || LISTA_HASHTAG_SEQ.CURRVAL
-FROM dual
+       'HASHTAG' || LISTA_HASHTAG_SEQ.CURRVAL
+FROM DUAL
 CONNECT BY LEVEL <= 15;
 
 
 --MENSAJE
--- Utilizando la secuencia MENSAJE_SEQ para generar valores de ID
--- y generando valores aleatorios para las columnas ID_USUARIO_EMISOR e ID_USUARIO_RECEPTOR
+-- UTILIZANDO LA SECUENCIA MENSAJE_SEQ PARA GENERAR VALORES DE ID
+-- Y GENERANDO VALORES ALEATORIOS PARA LAS COLUMNAS ID_USUARIO_EMISOR E ID_USUARIO_RECEPTOR
 INSERT INTO MENSAJE (ID, CONTENIDO, ID_USUARIO_EMISOR, ID_USUARIO_RECEPTOR)
 SELECT MENSAJE_SEQ.NEXTVAL,
-       'Contenido' || MENSAJE_SEQ.CURRVAL,
-       TRUNC(DBMS_RANDOM.value(1, 20)) AS ID_USUARIO_EMISOR,
-       TRUNC(DBMS_RANDOM.value(1, 20)) AS ID_USUARIO_RECEPTOR
-FROM dual
+       'CONTENIDO' || MENSAJE_SEQ.CURRVAL,
+       TRUNC(DBMS_RANDOM.VALUE(1, 20)) AS ID_USUARIO_EMISOR,
+       TRUNC(DBMS_RANDOM.VALUE(1, 20)) AS ID_USUARIO_RECEPTOR
+FROM DUAL
 CONNECT BY LEVEL <= 40;
 
 
 --PUBLICACION
 DECLARE
-    v_id NUMBER;
-    v_contenido VARCHAR2(200);
-    v_fecha DATE;
-    v_id_lista_temas NUMBER;
-    v_id_usuario NUMBER;
+    V_ID NUMBER;
+    V_CONTENIDO VARCHAR2(200);
+    V_FECHA DATE;
+    V_ID_LISTA_TEMAS NUMBER;
+    V_ID_USUARIO NUMBER;
 BEGIN
-    -- Iniciar bucle FOR
-    FOR i IN 1..20 LOOP
-        -- Generar datos aleatorios
-        v_id := i;
-        v_fecha := SYSDATE - DBMS_RANDOM.VALUE(1, 365); -- Fecha aleatoria en el último año
-        v_id_lista_temas := TRUNC(DBMS_RANDOM.VALUE(1, 6));
-        v_id_usuario := TRUNC(DBMS_RANDOM.VALUE(1, 21)); -- Rango de valores de 1 a 20
-        v_contenido := 'Contenido' || TO_CHAR(i);
+    -- INICIAR BUCLE FOR
+    FOR I IN 1..20 LOOP
+        -- GENERAR DATOS ALEATORIOS
+        V_ID := I;
+        V_FECHA := SYSDATE - DBMS_RANDOM.VALUE(1, 365); -- FECHA ALEATORIA EN EL ï¿½LTIMO Aï¿½O
+        V_ID_LISTA_TEMAS := TRUNC(DBMS_RANDOM.VALUE(1, 6));
+        V_ID_USUARIO := TRUNC(DBMS_RANDOM.VALUE(1, 21)); -- RANGO DE VALORES DE 1 A 20
+        V_CONTENIDO := 'CONTENIDO' || TO_CHAR(I);
 
-        -- Insertar datos en la tabla
-        INSERT INTO PUBLICACION (ID, Fecha, ID_LISTA_TEMAS, ID_USUARIO, Contenido)
-        VALUES (PUBLICACION_SEQ.NEXTVAL, v_fecha, v_id_lista_temas, v_id_usuario, v_contenido);
+        -- INSERTAR DATOS EN LA TABLA
+        INSERT INTO PUBLICACION (ID, FECHA, ID_LISTA_TEMAS, ID_USUARIO, CONTENIDO)
+        VALUES (PUBLICACION_SEQ.NEXTVAL, V_FECHA, V_ID_LISTA_TEMAS, V_ID_USUARIO, V_CONTENIDO);
     END LOOP;
 
-    -- Commit para confirmar las inserciones
+    -- COMMIT PARA CONFIRMAR LAS INSERCIONES
     COMMIT;
 END;
 /
 
 
 --HASHTAG_PUBLICACION
--- Utilizando la secuencia HASHTAG_PUBLICACION_SEQ para generar valores de ID
--- y generando valores aleatorios para las columnas ID_HASHTAG e ID_PUBLICACION
+-- UTILIZANDO LA SECUENCIA HASHTAG_PUBLICACION_SEQ PARA GENERAR VALORES DE ID
+-- Y GENERANDO VALORES ALEATORIOS PARA LAS COLUMNAS ID_HASHTAG E ID_PUBLICACION
 INSERT INTO HASHTAG_PUBLICACION (ID, ID_HASHTAG, ID_PUBLICACION)
 SELECT HASHTAG_PUBLICACION_SEQ.NEXTVAL,
-       TRUNC(DBMS_RANDOM.value(1, 15)) AS ID_HASHTAG,
-       TRUNC(DBMS_RANDOM.value(9, 21)) AS ID_PUBLICACION
-FROM dual
+       TRUNC(DBMS_RANDOM.VALUE(1, 15)) AS ID_HASHTAG,
+       TRUNC(DBMS_RANDOM.VALUE(9, 21)) AS ID_PUBLICACION
+FROM DUAL
 CONNECT BY LEVEL <= 12;
 
 
 --NOTICIA
 DECLARE
-    v_id NUMBER;
-    v_titular VARCHAR2(100);
-    v_fuente VARCHAR2(100);
-    v_id_publicacion NUMBER;
+    V_ID NUMBER;
+    V_TITULAR VARCHAR2(100);
+    V_FUENTE VARCHAR2(100);
+    V_ID_PUBLICACION NUMBER;
 BEGIN
-    -- Iniciar bucle FOR
-    FOR i IN 1..8 LOOP
-        -- Generar datos aleatorios
-        v_id := i;
-        v_titular := 'Noticia' || TO_CHAR(i);
-        v_fuente := 'Fuente' || TO_CHAR(i);
-        v_id_publicacion := TRUNC(DBMS_RANDOM.VALUE(1, 9)); -- Número aleatorio entre 1 y 8
+    -- INICIAR BUCLE FOR
+    FOR I IN 1..8 LOOP
+        -- GENERAR DATOS ALEATORIOS
+        V_ID := I;
+        V_TITULAR := 'NOTICIA' || TO_CHAR(I);
+        V_FUENTE := 'FUENTE' || TO_CHAR(I);
+        V_ID_PUBLICACION := TRUNC(DBMS_RANDOM.VALUE(1, 9)); -- Nï¿½MERO ALEATORIO ENTRE 1 Y 8
 
-        -- Insertar datos en la tabla
-        INSERT INTO NOTICIA (ID, Titular, Fuente, ID_publicacion)
-        VALUES (NOTICIA_SEQ.NEXTVAL, v_titular, v_fuente, v_id_publicacion);
+        -- INSERTAR DATOS EN LA TABLA
+        INSERT INTO NOTICIA (ID, TITULAR, FUENTE, ID_PUBLICACION)
+        VALUES (NOTICIA_SEQ.NEXTVAL, V_TITULAR, V_FUENTE, V_ID_PUBLICACION);
     END LOOP;
 
-    -- Commit para confirmar las inserciones
+    -- COMMIT PARA CONFIRMAR LAS INSERCIONES
     COMMIT;
 END;
 /
-
-
-
-
-
 
 
